9ec1c2bb94cc8aa67cc0198908112b05ee1fe6b8
sidu1994 <106261319+sidu1994@users.noreply.github.com>
2022-07-31 17:19:29 +0900

    指摘箇所修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, except: [:index , :new , :create , :update]
+  before_action :set_prototype, except: [:index , :new , :create ]
   before_action :authenticate_user!,except: [:index, :show]
   before_action :contributor_confirmation, only:[:edit, :update, :destroy]
   def index
